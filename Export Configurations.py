86b2b5694fbfc39d6eb1b5c0760ef92a001557c6
Donutnz <josh118tb@gmail.com>
2024-12-05 18:07:35 +1300

    Added support for non-configured designs. And converted to tabs.

--- Export Configurations.py
+++ Export Configurations.py
@@ -9,103 +9,127 @@ headerAuthor=("JOSH T-B",)
 headerOrg=("THE METAL COMPANY",)
 headerAuth="S. FISHER & SONS LTD."
 
+def modifySTEP(fileName, app, partName):
+	try:
+		stepFile=p21.readfile(fileName)
+		sData:p21.DataSection=stepFile.data[0]
+		
+		oldHeader=stepFile.header.get("FILE_NAME")
+
+		if(oldHeader is not None):
+			fileNameInfo=[
+				oldHeader.params[0], #Name
+				oldHeader.params[1], #Timestamp
+				headerAuthor, #Author
+				headerOrg, #Organization
+				oldHeader.params[4], #Preprocessor version
+				oldHeader.params[5], #Originating System
+				headerAuth #Authorization
+				]
+			newFName=p21.entity("FILE_NAME", fileNameInfo)
+			stepFile.header.add(newFName)
+
+		# Replace PRODUCT and PRODUCT_DEFINITION. This makes the root component show up with the TMC preferred name for some CAD programs.
+		for k,v in sData.instances.items():
+			if isinstance(v, p21.SimpleEntityInstance):
+				#print(str(k)+":"+str(v.entity.name))
+				if v.entity.name == "PRODUCT" or v.entity.name == "PRODUCT_DEFINITION":
+					if(v.entity.params[0] == partName):
+						swpParams:list=list(v.entity.params)
+						swpParams[1]=v.entity.params[0]
+						repEntity=p21.simple_instance(v.ref, v.entity.name, swpParams)
+						sData.add(repEntity)
+
+		stepFile.save(fileName)
+		app.log("Modified step file: {}".format(fileName))
+
+	except IOError as e:
+		app.log("Error opening STEP file: {} with error: {}".format(e.filename, e))
+	except p21.ParseError as e:
+		app.log("Error parsing STEP file: {} with error: {}".format(e))
+
 def run(context):
-    ui = None
-    try:
-        app = adsk.core.Application.get()
-        ui  = app.userInterface
+	ui = None
+	try:
+		app = adsk.core.Application.get()
+		ui  = app.userInterface
 
-        design=adsk.fusion.Design.cast(app.activeProduct)
+		design=adsk.fusion.Design.cast(app.activeProduct)
 
-        # Check if this is a configured design.
-        if not design.isConfiguredDesign:
-            ui.messageBox("Error: The current design is not configured! This script can only run on configured designs.")
-            return
-        
-        # Get Output Folder
-        targetFolderPath=""
-        folderDlg=ui.createFolderDialog()
-        folderDlg.title="Choose Output Folder"
+		app.log("Starting Configs Exporter...")
 
-        dlgResult=folderDlg.showDialog()
-        if(dlgResult == adsk.core.DialogResults.DialogOK):
-            targetFolderPath=folderDlg.folder
-        else:
-            return
+		# Check if this is a configured design.
+		#if not design.isConfiguredDesign:
+		#    ui.messageBox("Error: The current design is not configured! This script can only run on configured designs.")
+		#    return
+		
+		# Get Output Folder
+		targetFolderPath=""
+		folderDlg=ui.createFolderDialog()
+		folderDlg.title="Choose Output Folder"
 
-        # Get each configuration
-        topTable = design.configurationTopTable
-        
-        # Export each configuration
-        exportMgr=design.exportManager
-        filesCnt=0
+		dlgResult=folderDlg.showDialog()
+		if(dlgResult == adsk.core.DialogResults.DialogOK):
+			targetFolderPath=folderDlg.folder
+		else:
+			return
 
-        app.log("Beginning exporting into {}".format(targetFolderPath))
+		# Export each configuration
+		exportMgr=design.exportManager
+		
+		# Get each configuration
+		filesCnt=0
 
-        app.log("Rows: {}".format(len(topTable.rows)))
+		app.log("Beginning exporting into {}".format(targetFolderPath))
 
-        for row in topTable.rows:
-            app.log("Starting: {}".format(row.name))
+		if design.isConfiguredDesign:
+			topTable = design.configurationTopTable
+			app.log("Exporting Configured Design Rows: {}".format(topTable.rows.count))
 
-            rowActSuccess=row.activate()
+			app.log("Rows: {}".format(len(topTable.rows)))
 
-            if not rowActSuccess:
-                app.log("Failed to activate row")
-                raise Exception("Error activating row: {}".format(row.name))
+			for row in topTable.rows:
+				app.log("Starting: {}".format(row.name))
 
-            fileName=targetFolderPath+'/'+row.name+".step"
+				rowActSuccess=row.activate()
 
-            #rootObj=design.activeComponent
+				if not rowActSuccess:
+					app.log("Failed to activate row")
+					raise Exception("Error activating row: {}".format(row.name))
 
-            stepExpOpts=exportMgr.createSTEPExportOptions(fileName)
-            exportMgr.execute(stepExpOpts)
+				fileName=targetFolderPath+'/'+row.name+".step"
 
-            app.log("Exported standard step file: {}".format(fileName))
+				#rootObj=design.activeComponent
 
-            #Reopen exported STEP file to tweak names and add header stuff.
-            try:
-                stepFile=p21.readfile(fileName)
-                sData:p21.DataSection=stepFile.data[0]
-                
-                oldHeader=stepFile.header.get("FILE_NAME")
+				stepExpOpts=exportMgr.createSTEPExportOptions(fileName)
+				exportMgr.execute(stepExpOpts)
 
-                if(oldHeader is not None):
-                    fileNameInfo=[
-                        oldHeader.params[0], #Name
-                        oldHeader.params[1], #Timestamp
-                        headerAuthor, #Author
-                        headerOrg, #Organization
-                        oldHeader.params[4], #Preprocessor version
-                        oldHeader.params[5], #Originating System
-                        headerAuth #Authorization
-                        ]
-                    newFName=p21.entity("FILE_NAME", fileNameInfo)
-                    stepFile.header.add(newFName)
+				app.log("Exported standard step file: {}".format(fileName))
 
-                # Replace PRODUCT and PRODUCT_DEFINITION. This makes the root component show up with the TMC preferred name for some CAD programs.
-                for k,v in sData.instances.items():
-                    if isinstance(v, p21.SimpleEntityInstance):
-                        #print(str(k)+":"+str(v.entity.name))
-                        if v.entity.name == "PRODUCT" or v.entity.name == "PRODUCT_DEFINITION":
-                            if(v.entity.params[0] == row.name):
-                                swpParams:list=list(v.entity.params)
-                                swpParams[1]=v.entity.params[0]
-                                repEntity=p21.simple_instance(v.ref, v.entity.name, swpParams)
-                                sData.add(repEntity)
+				#Reopen exported STEP file to tweak names and add header stuff.
+				modifySTEP(fileName, app, row.name)
+				
+				app.log("Done: {}\n".format(row.name))
+				filesCnt+=1
 
-                stepFile.save(fileName)
-                app.log("Modified step file: {}".format(fileName))
+		else:
+			app.log("Exporting Root Component")
+			fileName=targetFolderPath+'/'+design.rootComponent.partNumber+".step"
 
-            except IOError as e:
-                app.log("Error opening STEP file: {} with error: {}".format(e.filename, e))
-            except p21.ParseError as e:
-                app.log("Error parsing STEP file: {} with error: {}".format(e))
-            
-            app.log("Done: {}\n".format(row.name))
-            filesCnt+=1
+			#rootObj=design.activeComponent
 
-        ui.messageBox("Operation Complete. "+str(filesCnt)+" Step Files Exported to: "+targetFolderPath+".")
+			stepExpOpts=exportMgr.createSTEPExportOptions(fileName)
+			exportMgr.execute(stepExpOpts)
 
-    except:
-        if ui:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+			app.log("Exported standard step file: {}".format(fileName))
+
+			#Reopen exported STEP file to tweak names and add header stuff.
+			modifySTEP(fileName, app, design.rootComponent.partNumber)
+			filesCnt+=1
+
+		ui.messageBox("Operation Complete. "+str(filesCnt)+" Step File/s Exported to: "+targetFolderPath+".")
+		app.log("Done")
+
+	except:
+		if ui:
+			ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
